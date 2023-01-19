9d72ebd1efe1d9e0bd2925bf3c5d5b350acd21c2
hanafichoi <hanafichoirulloh1@gmail.com>
2023-01-20 02:45:10 +0700

    initial commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text className="text-white text-2xl bg-red-400">HomeScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
